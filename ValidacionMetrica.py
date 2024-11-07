23198d21966c07d3b2acffa02eebd70cc2219ae0
gorrego <gabriel0320@gmail.com>
2024-11-06 21:57:41 -0500

    adicionar validacion metrica

---
+++ ValidacionMetrica.py
@@ -0,0 +1,29 @@
+import sklearn
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Cargar el conjunto de datos Iris
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+# Crear el clasificador de vecinos más cercanos
+clf = KNeighborsClassifier(n_neighbors=3)
+# Entrenar el clasificador
+clf.fit(X_train, y_train)
+# Predecir las etiquetas para los dtos de prueba
+y_pred = clf.predict(X_test)
+# Calcular la precisión del clasificador
+accuracy = accuracy_score(y_test, y_pred)
+print('Precisión del clasificador:', accuracy)
+
+#Calcular metricas de evaluacion del modelo
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+f1 = f1_score(y_test, y_pred, average='weighted')
+
+print("Precision:", accuracy)
+print('Precicion promedio ponderada:', precision)
+print('Recall promedio ponderado:', recall)
+print('F1 Score promedio ponderado:', f1)
