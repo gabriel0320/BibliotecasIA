fef119f7fc7590537cd43d79f320e2837d5359f3
gorrego <gorrego@bancolombia.com.co>
2024-11-13 22:03:30 -0500

    adcionando reto y mejorando lectura y guardado de peliculas desde archivo de texto

--- main.py
+++ main.py
@@ -1,52 +1,30 @@
 from fastapi import FastAPI,Query # Importamos la libreria FastAPI
 from fastapi.responses import JSONResponse # Importamos la libreria JSONResponse
-#from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse
 from typing import Optional
 
 import nltk # Importamos la libreria Optional para volver parametros opcionales
+import json
 
-movies_list = [
-    {
-        "id": 1,
-        "title": "Inception",
-        "overview": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
-        "year": 2010,
-        "rating": 8.8,
-        "category": "action"
-    },
-    {
-        "id": 2,
-        "title": "The Shawshank Redemption",
-        "overview": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        "year": 1994,
-        "rating": 9.3,
-        "category": "drama"
-    },
-    {
-        "id": 3,
-        "title": "The Godfather",
-        "overview": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-        "year": 1972,
-        "rating": 9.2,
-        "category": "action"
-    },
-    {
-        "id": 4,
-        "title": "The Dark Knight",
-        "overview": "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.",
-        "year": 2008,
-        "rating": 9.0,
-        "category": "action"
-    },
-    {
-        "id": 5,
-        "title": "Pulp Fiction",
-        "overview": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-        "year": 1994,
-        "rating": 8.9,
-        "category": "fiction"
-    }
-]
+
+def leer_peliculas_desde_json(archivo_json):
+    with open(archivo_json, 'r', encoding='utf-8') as file:
+        peliculas = json.load(file)
+    return peliculas
+
+# Uso de la función
+archivo_json = 'movies.json'
+movies_list = leer_peliculas_desde_json(archivo_json)
+
+
+
+def guardar_peliculas_en_json(lista_peliculas, archivo_json):
+    with open(archivo_json, 'w', encoding='utf-8') as file:
+        json.dump(lista_peliculas, file, ensure_ascii=False, indent=4)
+
+# Uso de las funciones
+archivo_json = 'movies.json'
+
 
 
 app = FastAPI() # Creamos una instancia de la libreria FastAPI
@@ -79,6 +57,7 @@ def get_movies_by_category(category:Optional[str],year:Optional[int]): # Decorad
 @app.post("/movies/",tags=["Movies"]) # Decorador para indicar que es una ruta de la API
 def add_movie(movie:dict): # Funcion que retorna un mensaje
     movies_list.append(movie)
+    guardar_peliculas_en_json(movies_list, archivo_json)
     return JSONResponse(content={"message": "Pelicula agregada"})
 #PUT para actualizar pelicula dado un json
 @app.put("/movies/",tags=["Movies"]) # Decorador para indicar que es una ruta de la API
@@ -98,13 +77,12 @@ def delete_movie(id:int): # Funcion que retorna un mensaje
     return JSONResponse(content={"message": "Pelicula no encontrada"})
 
 #Tokenizar
+@app.post("/tokenize") # Decorador para indicar que es una ruta de la API
+def tokenize(text:str): # Funcion que retorna un mensaje
+    return preprocessar(text)
 
-@app.get("/movies/tokenize")
-def tokenize(text: str = Query(...)):
-    processed_text = preprocesar(text)
-    return {"processed_text": processed_text}
 
-def preprocesar(text):
+def preprocessar(text):
     import json  # Importamos la librería json para trabajar con archivos json
     from nltk.tokenize import word_tokenize
     import nltk
@@ -112,7 +90,7 @@ def preprocesar(text):
     tokens = word_tokenize(text)
     result = {word: True for word in tokens}
     print(result)
-    return json.dumps(result)
+    return JSONResponse(content={"message":result})
 
 # para correr la app: uvicorn main:app --reload
 # uvicorn nombreApp:app --reload --port 5000 
